b4de46fa08719da470869667262a364e708b582c
alexandr <svaluhinav@yandex.ru>
2022-11-23 16:18:48 +0500

    feature/task1293

---
+++ src/timus/task_1293/Main.java
@@ -0,0 +1,20 @@
+package timus.task_1293;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner((System.in));
+        PrintWriter out = new PrintWriter(System.out);
+
+        int N= in.nextInt();
+        int A= in.nextInt();
+        int B= in.nextInt();
+
+        int result= (N* (A*B))*2;
+        out.println(result);
+        out.flush();
+    }
+}
