d765fa61d6a84a338946a6b733eb648c8a346d1f
alexandr <svaluhinav@yandex.ru>
2022-11-23 14:49:58 +0500

    commit Example12

---
+++ src/lab1/Example12.java
@@ -0,0 +1,18 @@
+package lab1;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class Example12 {
+    public static void main(String[] args) {
+
+        LocalDate today = LocalDate.now();
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Введите ваш возраст");
+        int age = in.nextInt();
+
+        System.out.println("Ваш год рождения: "+ (today.getYear() - age));
+    }
+}
+
