c866a6403e64db899136a6f787a0050428927902
alexandr <svaluhinav@yandex.ru>
2022-11-23 15:07:59 +0500

    commit Example14

---
+++ src/lab1/Example14.java
@@ -0,0 +1,13 @@
+package lab1;
+
+import java.util.Scanner;
+
+public class Example14 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Введите число: ");
+        int a= in.nextInt();
+        System.out.println((a-1)+ " " +a+ " "+ (a+1)+ " "+(a*a));
+    }
+}
