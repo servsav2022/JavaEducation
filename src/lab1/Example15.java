0a029aa11481cca1004a48f844c49516d0791950
alexandr <svaluhinav@yandex.ru>
2022-11-23 15:11:20 +0500

    commit Example15

---
+++ src/lab1/Example15.java
@@ -0,0 +1,18 @@
+package lab1;
+
+import java.util.Scanner;
+
+public class Example15 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Введите число a ");
+        int a= in.nextInt();
+        System.out.println("Введите число b ");
+        int b= in.nextInt();
+
+        System.out.println("Сумма чисел a+b="+(a+b));
+        System.out.println("Сумма чисел a-b="+(a-b));
+    }
+}
