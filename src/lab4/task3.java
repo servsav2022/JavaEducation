b711a71bc3ed907d5dbef8e10858146579f15de8
alexandr <svaluhinav@yandex.ru>
2022-12-17 21:28:57 +0500

    lab4/task3

---
+++ src/lab4/task3.java
@@ -0,0 +1,16 @@
+package lab4;
+
+//3. Напишите программу, в которой создается двумерный массив, который выводи прямоугольник из цифр 2;
+//
+public class task3 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <6; i++) {
+            for (int j = 1; j < 12; j++) {
+                System.out.print("2 ");
+            }
+            System.out.println();
+        }
+
+    }
+}
