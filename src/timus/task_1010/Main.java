f00793769887ce544067be7fa708a3f686a64660
alexandr <svaluhinav@yandex.ru>
2023-01-08 22:25:23 +0500

    timus/1010

---
+++ src/timus/task_1010/Main.java
@@ -0,0 +1,25 @@
+package timus.task_1010;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        long k = 0;
+        int p1 = 0;
+        int p2 = 0;
+        int N = in.nextInt();
+        long prev = in.nextLong();
+        for (int i = 2; i <= N; i++) {
+            long current = in.nextLong();
+            if (Math.abs(current - prev) > k) {
+                k = Math.abs(current - prev);
+                p1 = i - 1;
+                p2 = i;
+            }
+            prev = current;
+        }
+        System.out.println(p1 + " " + p2);
+    }
+}
