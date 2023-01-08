35b381e13b9408e9549503df81a86f835adfdb23
alexandr <svaluhinav@yandex.ru>
2023-01-08 17:07:20 +0500

    timus/1264

---
+++ src/timus/task_1264/main.java
@@ -0,0 +1,15 @@
+package timus.task_1264;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n= in.nextInt();
+        int m= in.nextInt();
+        int numOfM=m+1;
+        int result=n*numOfM;
+        System.out.println(result);
+    }
+}
