653146c9afc345b27cb4943daf4a45d8eb8df4ed
alexandr <svaluhinav@yandex.ru>
2023-02-28 12:51:42 +0500

    example6/lab8

---
+++ src/lab8/example6/Example6.java
@@ -0,0 +1,31 @@
+package lab8.example6;
+
+import java.io.*;
+
+public class Example6 {
+    public static void main(String[] args) throws IOException {
+        Reader in=null; // можно сразу записать FileReader in=null;
+        Writer out=null; // можно сразу записать FileWriter out =null;
+        try {
+        in = new FileReader("E:\\MyFile1.txt"); // файл для чтения
+        out= new FileWriter("E:\\MyFile2.txt", true); // файл для записи
+       // разрешено добавление
+// Данные считываются и записываются побайтно, как и для
+// InputStream/OutputStream
+        int oneByte; // переменная, в которую считываются данные
+        while ((oneByte = in.read()) != -1) {
+            // out.write((char)oneByte); // запись с уничтожением ранее
+                                         // существующих данных
+            out.append((char)oneByte);   // запись с добавлением данных в конец
+            System.out.print((char)oneByte);
+        }
+        } catch (IOException e) {
+            System.out.println("Ошибка!!!! ");
+        }
+        finally{
+        in.close();
+        out.close();
+        }
+    }
+}
+
