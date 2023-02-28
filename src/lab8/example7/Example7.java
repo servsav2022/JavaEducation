4e7386871754f38418828bf98531dc7eabd92df9
alexandr <svaluhinav@yandex.ru>
2023-02-28 13:03:55 +0500

    example7/lab8

---
+++ src/lab8/example7/Example7.java
@@ -0,0 +1,31 @@
+package lab8.example7;
+
+import java.io.*;
+
+public class Example7 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = null;
+        BufferedWriter out = null;
+    try{
+    // Создание файловых симовольных потоков для чтения и записи
+        br = new BufferedReader(new FileReader("E:\\MyFile1.txt"), 1024);
+        out = new BufferedWriter(new FileWriter("E:\\MyFile2.txt"));
+        int lineCount=0; // счетчик строк
+        String s;
+    // Переписывание информации из одного файла в другой
+        while ((s=br.readLine())!= null) {
+            lineCount++;
+            System.out.println(lineCount+": "+s);
+            out.write(s);
+            out.newLine();//переход на новую строку
+        }
+    } catch (IOException e){
+        System.out.println("Ошибка!!!!!");
+    }
+    finally {
+        br.close();
+        out.flush();
+        out.close();
+    }
+    }
+}
