4c3e8b63c17ef9294716f7f165b237810ce98f60
alexandr <svaluhinav@yandex.ru>
2023-03-06 14:48:10 +0500

    пример 1 /лаб 9

---
+++ src/lab9/Example1.java
@@ -0,0 +1,13 @@
+package lab9;
+
+public class Example1 {
+    public static void main(String[] args) {
+        try {
+            System.out.println("0");
+            throw  new RuntimeException("Непроверяемая ошибка");
+        } catch (RuntimeException e) { // исключение перехвачено
+            System.out.println("1  "+ e);  // исключение обработано
+        }
+        System.out.println("2");
+    }
+}
