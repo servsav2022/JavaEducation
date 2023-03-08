cf3d4f20531cc28177daa2257b11ec6b8eee9ece
alexandr <svaluhinav@yandex.ru>
2023-03-08 15:25:41 +0500

    пример 6 /лаб 9

---
+++ src/lab9/Example6.java
@@ -0,0 +1,20 @@
+package lab9;
+
+public class Example6 {
+    public static void main(String[] args) {
+        try {
+            System.out.println("0");
+            throw new NullPointerException("ошибка");
+        } catch (ArithmeticException e) {
+            System.out.println("1");
+        } catch (Exception e) {
+            System.out.println("2");
+        }
+        /*
+        catch (RuntimeException e) {
+            System.out.println("3");
+         }
+        */
+        System.out.println("4");
+    }
+}
