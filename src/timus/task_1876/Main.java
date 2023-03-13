41a2416cb2f48d3354f970c965b43ed9e1cb333f
alexandr <svaluhinav@yandex.ru>
2023-03-13 12:15:35 +0500

    timus 1876

---
+++ src/timus/task_1876/Main.java
@@ -0,0 +1,16 @@
+package timus.task_1876;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int left = scanner.nextInt();
+        int right = scanner.nextInt();
+        int case1 = 40 + right * 2;
+        int case2 = 78 + 40 + (left - 40) * 2 + 1;
+
+        System.out.println(Math.max(case1, case2));
+    }
+}
